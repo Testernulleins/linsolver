3c4f73e27252be706f40621358ccbd9e191cd74e
Joschua Schmidt <joschmid@uni-bremen.de>
2018-05-22 23:17:34 +0200

    Make Gaussian Eliminate capable of the first test
    The Algorithmus is not yet capable of different dimensions from
    3x3. It only works for non-linear-dependent matrices with rows
    in the right "order.

--- solvers.py
+++ solvers.py
@@ -15,4 +15,24 @@ def gaussian_eliminate(aa, bb):
     """
     nn = aa.shape[0]
     xx = np.zeros((nn,), dtype=float)
+    
+    
+    # first loop
+    #for ii in range(0, aa.shape[0])
+    fac1 = aa[1,0]/aa[0,0]
+    fac2 = aa[2,0]/aa[0,0]
+    aa[1] = aa[1] - fac1*aa[0]
+    aa[2] = aa[2] - fac2*aa[0]
+    bb[1] = bb[1] - fac1*bb[0]
+    bb[2] = bb[2] - fac2*bb[0]
+    
+    # second loop
+    fac3 = aa[2,1]/aa[1,1]
+    aa[2] = aa[2] - fac3*aa[1]
+    bb[2] = bb[2] - fac3*bb[1]
+    
+    # calculate xx
+    xx[2] = bb[2]/aa[2,2]
+    xx[1] = (bb[1] - aa[1,2]*xx[2]) / aa[1,1]
+    xx[0] = (bb[0] - aa[0,2]*xx[2] - aa[0,1]*xx[1]) / aa[0,0]
     return xx
