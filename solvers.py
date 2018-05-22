f23b74f103ba54cf10eaa605bde05508002926df
Joschua Schmidt <joschmid@uni-bremen.de>
2018-05-22 11:16:17 +0200

    Add the content into py files and .gitignore file
    The added content were given and not functual

--- solvers.py
+++ solvers.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 22 10:41:54 2018
+"""Routines for solving a linear system of equations."""
+import numpy as np
 
-@author: Joschua
-"""
 
+def gaussian_eliminate(aa, bb):
+    """Solves a linear system of equations (Ax = b) by Gauss-elimination
+
+    Args:
+        aa: Matrix with the coefficients. Shape: (n, n).
+        bb: Right hand side of the equation. Shape: (n,)
+
+    Returns:
+        Vector xx with the solution of the linear equation or None
+        if the equations are linearly dependent.
+    """
+    nn = aa.shape[0]
+    xx = np.zeros((nn,), dtype=float)
+    return xx
