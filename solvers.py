c48e029b482b54b458a2706dec5f34d8b9329073
Joschua Schmidt <joschmid@uni-bremen.de>
2018-05-22 10:49:22 +0200

    Add the empty py files
    The new files are empty and are named solvers.py and
    test_solvers.py

---
+++ solvers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 10:41:54 2018
+
+@author: Joschua
+"""
+
---
+++ test_solvers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 10:42:37 2018
+
+@author: Joschua
+"""
+
