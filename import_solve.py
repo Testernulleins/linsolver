4a8932a6d30fbc1dc8992f9fb26fec4b17ee2cc3
Joschua Schmidt <joschmid@uni-bremen.de>
2018-06-19 10:07:48 +0200

    Create import_solve.py, not working yet
    Was only uploaded to be able to push all necessary files

---
+++ import_solve.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 12 11:40:12 2018
+
+@author: Joschua
+"""
+
+import numpy as np
+import solvers
+
+def read_matrix(fname):
+    file = open(fname, 'r')
+    nn = int(file.readline())
+    for line in range
+    tmp = np.loadtxt(fname)
+    file.close()
+
+print(read_matrix("linsolve.in"))
