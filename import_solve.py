7c2a1d9ecb8d2bc0c9406d5701bdb19de3e06231
Joschua Schmidt <joschmid@uni-bremen.de>
2018-06-19 10:44:17 +0200

    Make the import_solve.py working
    The result will now be printed

--- import_solve.py
+++ import_solve.py
@@ -10,10 +10,20 @@ import numpy as np
 import solvers
 
 def read_matrix(fname):
+    ''' This function imports a matrix out of a given fname.txt file'''
     file = open(fname, 'r')
-    nn = int(file.readline())
-    for line in range
-    tmp = np.loadtxt(fname)
+    out = []
+    lines = file.readlines()
+    for line in lines:
+        out.append(line.split())
     file.close()
 
-print(read_matrix("linsolve.in"))
+    nn = int(out[0][0])
+    aa = np.asarray(out[1:nn+1], dtype=float)
+    bb = np.asarray(out[nn+1], dtype=float)
+    return aa, bb
+
+
+aa, bb = read_matrix("linsolve.in")
+xx_gauss = solvers.gaussian_eliminate(aa, bb)
+print(xx_gauss)
